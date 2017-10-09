d39c880972bc62a416f181da37360eb6d1e0abe2
Eriel Miquilino <desenvolvimento1@hsiautomacao.com.br>
2017-10-09 10:13:40 -0300

    Classe de controle para grids

---
+++ src/main/java/br/com/hsi/controller/GridBean.java
@@ -0,0 +1,21 @@
+package br.com.hsi.controller;
+
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * @author Eriel Miquilino
+ */
+
+@Named
+@ViewScoped
+public class GridBean implements Serializable {
+
+    public void redrirect(String caminho) throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().redirect("/HSI/Sistemas/" + caminho + ".xhtml");
+    }
+
+}
